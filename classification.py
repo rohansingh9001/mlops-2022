c7235fd7b495fb8a1b6308dc0e1af6721df378cf
Rohan Singh <rohansingh9001@gmail.com>
2022-09-25 05:43:45 +0530

    quiz2

--- classification.py
+++ classification.py
@@ -5,7 +5,7 @@ from sklearn import datasets, svm, metrics
 from sklearn.model_selection import train_test_split
 
 digits = datasets.load_digits()
-
+print(digits.images.shape)
 _, axes = plt.subplots(nrows=1, ncols=4, figsize=(10, 3))
 for ax, image, label in zip(axes, digits.images, digits.target):
     ax.set_axis_off()
@@ -16,34 +16,55 @@ for ax, image, label in zip(axes, digits.images, digits.target):
 n_samples = len(digits.images)
 data = digits.images.reshape((n_samples, -1))
 
-# Create a classifier: a support vector classifier
-clf = svm.SVC(gamma=0.001)
+parameters = []
+accuracies = []
 
-# Split data into 50% train and 50% test subsets
-X_train, X_test, y_train, y_test = train_test_split(
-    data, digits.target, test_size=0.5, shuffle=False
-)
+for i in range(4):
+	for j in range(3):
+		parameters.append((0.0008 + 0.0001*i, 0.8 + 0.1*i))
+print("parameters:", parameters)
 
-# Learn the digits on the train subset
-clf.fit(X_train, y_train)
+for parameter in parameters:
+	gamma, C = parameter
+	# Create a classifier: a support vector classifier
+	clf = svm.SVC(gamma=gamma, C=C)
 
-# Predict the value of the digit on the test subset
-predicted = clf.predict(X_test)
+	# Split data into 50% train and 50% test subsets
+	X_train, X_test, y_train, y_test = train_test_split(
+	    data, digits.target, test_size=0.5, shuffle=False
+	)
 
-_, axes = plt.subplots(nrows=1, ncols=4, figsize=(10, 3))
-for ax, image, prediction in zip(axes, X_test, predicted):
-    ax.set_axis_off()
-    image = image.reshape(8, 8)
-    ax.imshow(image, cmap=plt.cm.gray_r, interpolation="nearest")
-    ax.set_title(f"Prediction: {prediction}")
+	# Learn the digits on the train subset
+	clf.fit(X_train, y_train)
 
-print(
-    f"Classification report for classifier {clf}:\n"
-    f"{metrics.classification_report(y_test, predicted)}\n"
-)
+	# Predict the value of the digit on the test subset
+	predicted2 = clf.predict(X_test)
 
-disp = metrics.ConfusionMatrixDisplay.from_predictions(y_test, predicted)
-disp.figure_.suptitle("Confusion Matrix")
-print(f"Confusion matrix:\n{disp.confusion_matrix}")
+	#_, axes = plt.subplots(nrows=1, ncols=4, figsize=(10, 3))
+	#for ax, image, prediction in zip(axes, X_test, predicted):
+	#    ax.set_axis_off()
+	#    image = image.reshape(8, 8)
+	#    ax.imshow(image, cmap=plt.cm.gray_r, interpolation="nearest")
+	#    ax.set_title(f"Prediction: {prediction}")
+	accuracy2 = metrics.accuracy_score(y_test, predicted2)
+	predicted1 = clf.predict(X_train)
+	accuracy1 = metrics.accuracy_score(y_train, predicted1)
+	accuracies.append((accuracy1, accuracy2))
 
-plt.show()
+	# disp = metrics.ConfusionMatrixDisplay.from_predictions(y_test, predicted)
+	# disp.figure_.suptitle("Confusion Matrix")
+	# print(f"Confusion matrix:\n{disp.confusion_matrix}")
+
+	# plt.show()
+print("gamma    C    train_acc    test_acc")
+for parameter, accuracy in zip(parameters, accuracies):
+	gamma, C = parameter
+	train_acc, test_acc = accuracy
+	print("%.4f" % gamma, '|', "%.1f" % C, '|', "%.7f" % train_acc, '|', "%.6f" % test_acc)
+
+test_acc = [b for a, b in accuracies]
+test_acc.sort()
+print("max accuracy:", max(test_acc))
+print("min accuracy:", min(test_acc))
+print("mean accuracy:", sum(test_acc)/len(test_acc))
+print("median accuracy:", test_acc[len(test_acc)//2])
