dd4fd413e61e0986f7a6e9ab950002c35d045abf
Rohan Singh <rohansingh9001@gmail.com>
2022-10-30 23:18:37 +0530

    assignment 4

--- classification.py
+++ classification.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
-
+import math
 # Import datasets, classifiers and performance metrics
-from sklearn import datasets, svm, metrics
+from sklearn import datasets, svm, tree, metrics
 from sklearn.model_selection import train_test_split
 
 digits = datasets.load_digits()
@@ -16,34 +16,54 @@ for ax, image, label in zip(axes, digits.images, digits.target):
 n_samples = len(digits.images)
 data = digits.images.reshape((n_samples, -1))
 
-# Create a classifier: a support vector classifier
-clf = svm.SVC(gamma=0.001)
+svm_accuracies = []
+print("SVM")
+for i in range(5):
+    # Create a classifier: a support vector classifier
+    clf = svm.SVC(gamma=0.001)
 
-# Split data into 50% train and 50% test subsets
-X_train, X_test, y_train, y_test = train_test_split(
-    data, digits.target, test_size=0.5, shuffle=False
-)
+    # Split data into 50% train and 50% test subsets
+    X_train, X_test, y_train, y_test = train_test_split(
+    data, digits.target, test_size=0.2, shuffle=True
+    )
 
-# Learn the digits on the train subset
-clf.fit(X_train, y_train)
+    # Learn the digits on the train subset
+    clf.fit(X_train, y_train)
 
-# Predict the value of the digit on the test subset
-predicted = clf.predict(X_test)
+    # Predict the value of the digit on the test subset
+    predicted = clf.predict(X_test)
 
-_, axes = plt.subplots(nrows=1, ncols=4, figsize=(10, 3))
-for ax, image, prediction in zip(axes, X_test, predicted):
-    ax.set_axis_off()
-    image = image.reshape(8, 8)
-    ax.imshow(image, cmap=plt.cm.gray_r, interpolation="nearest")
-    ax.set_title(f"Prediction: {prediction}")
+    acc = metrics.accuracy_score(y_test, predicted)
+    print(acc)
+    svm_accuracies.append(acc)
 
-print(
-    f"Classification report for classifier {clf}:\n"
-    f"{metrics.classification_report(y_test, predicted)}\n"
-)
+svm_mean = sum(svm_accuracies)/5
+svm_std = math.sqrt(sum([(i-svm_mean)**2 for i in svm_accuracies])/5)
+print("SVM Mean: ", svm_mean)
+print("SVM std: ", svm_std, "\n")
 
-disp = metrics.ConfusionMatrixDisplay.from_predictions(y_test, predicted)
-disp.figure_.suptitle("Confusion Matrix")
-print(f"Confusion matrix:\n{disp.confusion_matrix}")
+tree_accuracies = []
+print("Decision Tree")
+for i in range(5):
+    # Create a classifier: a support vector classifier
+    clf = tree.DecisionTreeClassifier(max_features=500, max_depth=70, max_leaf_nodes=1200)
 
-plt.show()
+    # Split data into 50% train and 50% test subsets
+    X_train, X_test, y_train, y_test = train_test_split(
+    data, digits.target, test_size=0.2, shuffle=True
+    )
+
+    # Learn the digits on the train subset
+    clf.fit(X_train, y_train)
+
+    # Predict the value of the digit on the test subset
+    predicted = clf.predict(X_test)
+
+    acc = metrics.accuracy_score(y_test, predicted)
+    print(acc)
+    tree_accuracies.append(acc)
+
+tree_mean = sum(tree_accuracies)/5
+tree_std = math.sqrt(sum([(i-tree_mean)**2 for i in tree_accuracies])/5)
+print("Tree Mean: ", tree_mean)
+print("Tree std: ", tree_std, "\n")
